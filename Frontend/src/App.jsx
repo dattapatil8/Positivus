8e56c962a13b3571bdcdc54867a4914705df12cd
Maruti96 <ymaruti96@gmail.com>
2026-04-03 08:50:39 +0000

    Services initilize

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react"
 import {Home} from "./Container/Home"
+import { Services } from "./Container/Services"
 import { CaseStudies } from "./Container/CaseStudies"
+
 function App() {
   return (
     <>
       <Home />
+      <Services />
       <CaseStudies />
     </>
   )
--- Frontend/src/Container/Home.jsx
+++ Frontend/src/Container/Home.jsx
@@ -3,6 +3,7 @@ export function Home(){
     return(
         <>
         <h1> Home Page</h1>
+        
         </>
     )
 }
