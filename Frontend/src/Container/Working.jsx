3df0bf19f534581460fa6f4a42d1301a1e426a99
Dattatray patil <dattapatil862002@gmail.com>
2026-04-03 11:22:45 +0000

    Worcking page update

--- Frontend/src/Container/Working.jsx
+++ Frontend/src/Container/Working.jsx
@@ -1,8 +1,52 @@
 export function Working(){
     return(
         <>
-        <h1>hello</h1>
+        <div>
+            <div className="flex">
+            <h1 className="text-2xl ml-7 bg-[rgba(185,255,102,1)] w-70 text-center h-10"><b>Our Working Process</b> </h1>
+            <h4 className="ml-10  ">Step-by-Step Guide to Achieving <br></br>Your Business Goals</h4>
+            </div>
+            <div className="w-280 h-40 m-10 hover:bg-[rgba(185,255,102,1)] border-2 rounded-3xl shadow-xl/30"> 
+                <div className="flex h-15">
+                    <h1 className="text-2xl ml-8 mt-5"><b>01</b></h1>
+                    <h3 className="mt-7 ml-2"><b>Consultation</b></h3>  
+                </div>
+                <div>
+                    <hr className="w-260 ml-10"></hr>
+                    <h6 className="m-5">During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.</h6>
+                </div>
+            </div>
+             <div className="w-280 h-20 m-10 hover:bg-[rgba(185,255,102,1)] border-2 rounded-3xl shadow-xl/30"> 
+                <div className="flex h-15">
+                    <h1 className="text-2xl ml-8 mt-5"><b>02</b></h1>
+                    <h3 className="mt-7 ml-2"><b>Research and Strategy Development</b></h3>  
+                </div>
+                </div>
+                <div className="w-280 h-20 m-10 hover:bg-[rgba(185,255,102,1)] border-2 rounded-3xl shadow-xl/30"> 
+                <div className="flex h-15">
+                    <h1 className="text-2xl ml-8 mt-5"><b>03</b></h1>
+                    <h3 className="mt-7 ml-2"><b>Implementation</b></h3>  
+                </div>
+                </div>
+                <div className="w-280 h-20 m-10 hover:bg-[rgba(185,255,102,1)] border-2 rounded-3xl shadow-xl/30"> 
+                <div className="flex h-15">
+                    <h1 className="text-2xl ml-8 mt-5"><b>04</b></h1>
+                    <h3 className="mt-7 ml-2"><b>Monitoring and Optimization</b></h3>  
+                </div>
+                </div>
+                <div className="w-280 h-20 m-10 hover:bg-[rgba(185,255,102,1)] border-2 rounded-3xl shadow-xl/30"> 
+                <div className="flex h-15">
+                    <h1 className="text-2xl ml-8 mt-5"><b>05</b></h1>
+                    <h3 className="mt-7 ml-2"><b>Reporting and Communication</b></h3>  
+                </div>
+                </div>
+                <div className="w-280 h-20 m-10 hover:bg-[rgba(185,255,102,1)] border-2 rounded-3xl shadow-xl/30"> 
+                <div className="flex h-15">
+                    <h1 className="text-2xl ml-8 mt-5"><b>06</b></h1>
+                    <h3 className="mt-7 ml-2"><b>Continual Improvement</b></h3>  
+                </div>
+                </div>
+        </div>
         </>
     )
-    
 }
