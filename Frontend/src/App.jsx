2cda003235638a6e1dece336c576da0bfe90841f
Dipak Magadum <deepakmagdum769@gmail.com>
2026-04-03 11:55:20 +0000

    render home

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react"
-import {Home} from "./Container/Home"
+import {Home} from "./Container/Home.jsx"
 import { Services } from "./Container/Services"
 import { CaseStudies } from "./Container/CaseStudies"
 import { Working } from "./Container/Working"
-
 function App() {
   return (
     <>
