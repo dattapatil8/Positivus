a1ea264c937ce833f336ed00c7a6131998824d3a
Dattatray patil <dattapatil862002@gmail.com>
2026-04-04 05:51:12 +0000

    Adding a button

--- Frontend/src/Container/Working.jsx
+++ Frontend/src/Container/Working.jsx
@@ -2,14 +2,15 @@ export function Working(){
     return(
         <>
         <div>
-            <div className="flex">
-            <h1 className="text-2xl ml-7 bg-[rgba(185,255,102,1)] w-70 text-center h-10"><b>Our Working Process</b> </h1>
+            <div className="flex mt-10">
+            <h1 className="text-2xl  ml-7 bg-[rgba(185,255,102,1)] w-70 text-center h-10"><b>Our Working Process</b> </h1>
             <h4 className="ml-10  ">Step-by-Step Guide to Achieving <br></br>Your Business Goals</h4>
             </div>
             <div className="w-280 h-40 m-10 hover:bg-[rgba(185,255,102,1)] border-2 rounded-3xl shadow-xl/30"> 
-                <div className="flex h-15">
+                <div className="flex h-20">
                     <h1 className="text-2xl ml-8 mt-5"><b>01</b></h1>
                     <h3 className="mt-7 ml-2"><b>Consultation</b></h3>  
+                    <button className="h-12 w-12 rounded-full border-2 ml-215 mt-3 bg-amber-50 hover:bg-amber-100 cursor-pointer">-</button>
                 </div>
                 <div>
                     <hr className="w-260 ml-10"></hr>
@@ -19,31 +20,37 @@ export function Working(){
              <div className="w-280 h-20 m-10 hover:bg-[rgba(185,255,102,1)] border-2 rounded-3xl shadow-xl/30"> 
                 <div className="flex h-15">
                     <h1 className="text-2xl ml-8 mt-5"><b>02</b></h1>
-                    <h3 className="mt-7 ml-2"><b>Research and Strategy Development</b></h3>  
+                    <h3 className="mt-7 ml-2  w-200 "><b>Research and Strategy Development</b></h3> 
+                    <button className="h-12 w-12 rounded-full border-2 ml-40 mt-3 bg-amber-50  hover:bg-amber-100 cursor-pointer">+</button> 
                 </div>
+                
                 </div>
                 <div className="w-280 h-20 m-10 hover:bg-[rgba(185,255,102,1)] border-2 rounded-3xl shadow-xl/30"> 
                 <div className="flex h-15">
                     <h1 className="text-2xl ml-8 mt-5"><b>03</b></h1>
-                    <h3 className="mt-7 ml-2"><b>Implementation</b></h3>  
+                    <h3 className="mt-7 ml-2  w-200 "><b>Implementation</b></h3> 
+                    <button className="h-12 w-12 rounded-full border-2 ml-40 mt-3 bg-amber-50  hover:bg-amber-100 cursor-pointer">+</button> 
                 </div>
                 </div>
                 <div className="w-280 h-20 m-10 hover:bg-[rgba(185,255,102,1)] border-2 rounded-3xl shadow-xl/30"> 
                 <div className="flex h-15">
                     <h1 className="text-2xl ml-8 mt-5"><b>04</b></h1>
-                    <h3 className="mt-7 ml-2"><b>Monitoring and Optimization</b></h3>  
+                    <h3 className="mt-7 ml-2  w-200 "><b>Monitoring and Optimization</b></h3>  
+                    <button className="h-12 w-12 rounded-full border-2 ml-40 mt-3 bg-amber-50  hover:bg-amber-100 cursor-pointer">+</button>
                 </div>
                 </div>
                 <div className="w-280 h-20 m-10 hover:bg-[rgba(185,255,102,1)] border-2 rounded-3xl shadow-xl/30"> 
                 <div className="flex h-15">
                     <h1 className="text-2xl ml-8 mt-5"><b>05</b></h1>
-                    <h3 className="mt-7 ml-2"><b>Reporting and Communication</b></h3>  
+                    <h3 className="mt-7 ml-2  w-200 "><b>Reporting and Communication</b></h3>  
+                    <button className="h-12 w-12 rounded-full border-2 ml-40 mt-3 bg-amber-50  hover:bg-amber-100 cursor-pointer">+</button>
                 </div>
                 </div>
                 <div className="w-280 h-20 m-10 hover:bg-[rgba(185,255,102,1)] border-2 rounded-3xl shadow-xl/30"> 
                 <div className="flex h-15">
                     <h1 className="text-2xl ml-8 mt-5"><b>06</b></h1>
-                    <h3 className="mt-7 ml-2"><b>Continual Improvement</b></h3>  
+                    <h3 className="mt-7 ml-2  w-200 "><b>Continual Improvement</b></h3>  
+                    <button className="h-12 w-12 rounded-full border-2 ml-40 mt-3 bg-amber-50  hover:bg-amber-100 cursor-pointer">+</button>
                 </div>
                 </div>
         </div>
