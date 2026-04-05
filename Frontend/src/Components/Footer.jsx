f6773553cad4a457d240b34e17001d0a772e4dd7
Dipak Magadum <deepakmagdum769@gmail.com>
2026-04-05 12:03:25 +0000

    create footer component

---
+++ Frontend/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import FooterImg from "../assets/Footer.png"
+function Footer(image) {
+  return (
+    <>
+    <div className="mt-5">
+        <img src={FooterImg} />
+    </div>
+    </>
+  )
+}
+
+export default Footer
