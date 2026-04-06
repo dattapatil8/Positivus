774b2ec022d4aad552258bd453f8d0be46f1c468
Dipak Magadum <deepakmagdum769@gmail.com>
2026-04-06 16:57:51 +0000

    add card

--- Frontend/src/Container/Teams.jsx
+++ Frontend/src/Container/Teams.jsx
@@ -1,14 +1,26 @@
+import SocialIcon from "../assets/SocialIcon.png"
+import Profile1 from "../assets/Profile1.png"
 export function Teams(){
 
     return(
         <>
         <div className="flex ">
-            <span className=" ml-10 bg-lime-300 px-3 py-1 font-semibold rounded" >Team</span>
+            <span className=" ml-10  h-10  text-center bg-lime-300 px-3 py-1 font-semibold rounded" >Team</span>
             <p className="ml-10"> Meet the skilled and experienced team behind our<br/>
                  successful digital marketing strategies</p>
         </div>
 
-        
+        <div className=" ml-10 mt-5 p-2 flex border rounded-2xl w-80 h-62">
+            <div className="">
+                <img className="mt-5 ml-60 w-10" 
+                src={SocialIcon} />
+
+                    <div className="flex ">
+                <img className="w-20"
+                 src={Profile1} />
+                 </div>
+            </div>   
+        </div>
         </>
     )
 }
