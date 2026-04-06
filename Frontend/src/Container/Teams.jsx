a8fc84ceabb7301dd8ca8bb077f4da62ac887760
Dipak Magadum <deepakmagdum769@gmail.com>
2026-04-06 15:56:00 +0000

    update Teams.jsx

--- Frontend/src/Container/Teams.jsx
+++ Frontend/src/Container/Teams.jsx
@@ -2,9 +2,13 @@ export function Teams(){
 
     return(
         <>
-        <div>
-            <span className="">Team</span>
+        <div className="flex ">
+            <span className=" ml-10 bg-lime-300 px-3 py-1 font-semibold rounded" >Team</span>
+            <p className="ml-10"> Meet the skilled and experienced team behind our<br/>
+                 successful digital marketing strategies</p>
         </div>
+
+        
         </>
     )
 }
