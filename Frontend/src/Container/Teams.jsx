66e4ef5e8e960b73ef97b2c29ee85e533d4d29c1
Dipak Magadum <deepakmagdum769@gmail.com>
2026-04-07 14:26:18 +0000

    call Card component

--- Frontend/src/Container/Teams.jsx
+++ Frontend/src/Container/Teams.jsx
@@ -1,5 +1,6 @@
 import SocialIcon from "../assets/SocialIcon.png"
 import Profile1 from "../assets/Profile1.png"
+import {Card} from "../Components/Card.jsx"
 export function Teams(){
 
     return(
@@ -9,7 +10,34 @@ export function Teams(){
             <p className="ml-10"> Meet the skilled and experienced team behind our<br/>
                  successful digital marketing strategies</p>
         </div>
-        
+       
+       <div className="flex justify-around">
+         <Card 
+        image={Profile1}
+        name="John Smith"
+        role="CEO and Founder"
+        detail="10+ years of experience in digital
+         marketing. Expertise in SEO, PPC,
+          and content strategy"
+        />
+
+         <Card 
+        image={Profile1}
+        name="John Smith"
+        role="CEO and Founder"
+        detail="10+ years of experience in digital
+         marketing. Expertise in SEO, PPC,
+          and content strategy"
+        />
+         <Card 
+        image={Profile1}
+        name="John Smith"
+        role="CEO and Founder"
+        detail="10+ years of experience in digital
+         marketing. Expertise in SEO, PPC,
+          and content strategy"
+        />
+       </div>
         </>
     )
 }
