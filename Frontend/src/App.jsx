a0184398eb98fc9435d73833477bf655fcf77bba
Dipak Magadum <deepakmagdum769@gmail.com>
2026-04-11 11:03:13 +0000

    import Routes and Route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import {Routes, Route} from "react-router-dom"
 import {Home} from "./Container/Home.jsx"
 import { Services } from "./Container/Services"
 import { CaseStudies } from "./Container/CaseStudies"
@@ -8,19 +9,23 @@ import {Testimonials} from "./Container/Testimonials.jsx"
 import { ContactUs } from "./Container/ContactUs.jsx"
 import Navbar from "./Components/Navbar";
 import { FooterBlock } from "./Container/FooterBlock.jsx"
+import { LandingPage } from "./Container/LandingPage.jsx"
 function App() {
   return (
     <>
-     <Navbar />
-
-      <Home />
-      <Services />
-      <CaseStudies />
-      <Working />
-      <Teams/>
-      <Testimonials/>
-      <ContactUs />
-      <FooterBlock />
+   
+    <Routes>
+      <Route path="/" element={<LandingPage/>} />
+    < Route path="/home"  element={<Home />} />
+     < Route path="/services"    element={ <Services />} /> 
+     < Route path="/casestudies"    element={<CaseStudies />}/>
+     < Route path="/working"    element={<Working />} />  
+     < Route path="/teams"    element={<Teams/>}  /> 
+     < Route path="/testimonials"    element={<Testimonials/>}  />
+     < Route path="/contactus"    element={<ContactUs />} />
+     < Route path="/footerblock"    element={<FooterBlock />}  />
+      </Routes>
+    
     </>
   )
 }
