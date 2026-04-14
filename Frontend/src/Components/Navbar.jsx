70a4a253ae915875a679539492fc8aabaffdfb14
Dipak Magadum <deepakmagdum769@gmail.com>
2026-04-14 14:48:35 +0000

    add button in navbar

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -9,17 +9,17 @@ function Navbar() {
 
       <div className=" flex text-lg gap-6 ">
 
-         <Link to="" className=" text-white cursor-pointer  p-3 hover:border  rounded ">About us</Link>
+         <Link to="" className=" text-white cursor-pointer  p-3  transition duration-300 hover:shadow-xl hover:border rounded ">About us</Link>
 
-         <Link to="/services" className=" text-white cursor-pointer  p-3 hover:border  rounded">Services</Link>
+         <Link to="/services" className=" text-white cursor-pointer  p-3 transition duration-300 hover:shadow-xl hover:border  rounded">Services</Link>
 
-         <Link to="/casestudies"className=" text-white cursor-pointer  p-3 hover:border  rounded">Use Cases</Link>
+         <Link to="/casestudies"className=" text-white cursor-pointer  p-3 transition duration-300 hover:shadow-xl hover:border  rounded">Use Cases</Link>
 
-         <Link to="" className=" text-white cursor-pointer  p-3 hover:border  rounded">Pricing</Link>
+         <Link to="" className=" text-white cursor-pointer  p-3 transition duration-300 hover:shadow-xl hover:border   rounded">Pricing</Link>
 
-         <Link to="" className=" text-white cursor-pointer  p-3 hover:border  rounded">Blog</Link>
+         <Link to="" className=" text-white cursor-pointer  p-3 transition duration-300 hover:shadow-xl hover:border   rounded">Blog</Link>
 
-         <Link  to="" className=" text-white cursor-pointer  p-3 hover:border  rounded">Request a quote</Link>
+         <button className="border rounded-lg p-2 cursor-pointer  text-white  tranition duration-300 hover:shadow-xl">Request a quote</button>
 
       </div>
        
