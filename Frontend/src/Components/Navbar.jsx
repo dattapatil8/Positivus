c6202c897bcee1e283f01def31c7229187e77e39
Dipak Magadum <deepakmagdum769@gmail.com>
2026-04-16 05:51:09 +0000

    style: change the font style

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -2,24 +2,24 @@ import Frame from "../assets/Frame.png"
 import {Link} from "react-router-dom"
 function Navbar() {
   return (
-    <nav className=" flex justify-between  items-center bg-green-600 px-4 py-4 w-full ">
+    <nav className=" flex justify-between  items-center  bg-[rgba(185,255,102,1)] px-4 py-4 w-full ">
     
         <img  src={Frame} />
       
 
       <div className=" flex text-lg gap-6 ">
 
-         <Link to="" className=" text-white text-xl cursor-pointer  p-3  transition duration-300 hover:shadow-xl hover:border rounded ">About us</Link>
+         <Link to="" className=" text-black font-semibold text-xl cursor-pointer  p-3  transition duration-300 hover:shadow-xl hover:border rounded ">About us</Link>
 
-         <Link to="/services" className=" text-white text-xl cursor-pointer  p-3 transition duration-300 hover:shadow-xl hover:border  rounded">Services</Link>
+         <Link to="/services" className=" text-black font-semibold text-xl cursor-pointer  p-3  transition duration-300 hover:shadow-xl hover:border rounded ">Services</Link>
 
-         <Link to="/casestudies"className=" text-white text-xl cursor-pointer  p-3 transition duration-300 hover:shadow-xl hover:border  rounded">Use Cases</Link>
+         <Link to="/casestudies" className=" text-black font-semibold text-xl cursor-pointer  p-3  transition duration-300 hover:shadow-xl hover:border rounded ">Use Cases</Link>
 
-         <Link to="" className=" text-white text-xl cursor-pointer  p-3 transition duration-300 hover:shadow-xl hover:border   rounded">Pricing</Link>
+         <Link to="" className=" text-black font-semibold text-xl cursor-pointer  p-3  transition duration-300 hover:shadow-xl hover:border rounded ">Pricing</Link>
 
-         <Link to="" className=" text-white text-xl cursor-pointer  p-3 transition duration-300 hover:shadow-xl hover:border   rounded">Blog</Link>
+         <Link to="" className=" text-black font-semibold text-xl cursor-pointer  p-3  transition duration-300 hover:shadow-xl hover:border rounded ">Blog</Link>
 
-         <button className="border rounded-lg p-2 cursor-pointer  text-white text-xl  tranition duration-300 hover:shadow-xl">Request a quote</button>
+         <button className=" text-black font-semibold text-xl cursor-pointer  p-3  transition duration-300 hover:shadow-xl hover:border rounded ">Request a quote</button>
 
       </div>
        
